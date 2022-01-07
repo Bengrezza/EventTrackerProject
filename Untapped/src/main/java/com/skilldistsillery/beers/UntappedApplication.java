ba8bb917df0fd0aed8eb03c7970202be3f829ace
Bengrezza <bengretza@gmail.com>
2022-01-07 15:49:57 -0700

    Configured JPA project dependency

---
+++ Untapped/src/main/java/com/skilldistsillery/beers/UntappedApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistsillery.beers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntappedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntappedApplication.class, args);
+	}
+
+}
---
+++ Untapped/src/test/java/com/skilldistsillery/beers/UntappedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistsillery.beers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UntappedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
